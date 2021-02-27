23130a0cb9217b9186f2953bbffaf97b4660a109
ksquaring <62213268+ksquaring@users.noreply.github.com>
2021-02-27 12:58:39 -0500

    Add files via upload
    Created About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,39 @@
 import React from "react";
+import icon from "./assets/img/icon.png";
+
+
 
 export default function About(props) {
-  return <div>About</div>;
+  return (
+  <body left-margin="20%">
+    
+<table border="4px solid black" padding-left="20%">
+  <tr>
+  <th colspan="2">About Us!</th>
+  </tr>
+  <tr>
+    <td font-size="20px" width="200px" height="200px" >
+      Kedar
+    </td>
+    <td>
+    	<img src = {icon} width="200px" height="200px" alt="image of team member"></img>
+    </td>
+
+  </tr>
+ 
+  <tr>
+    <td font-size="20px" width="200px" height="200px">
+      Radek
+    </td>
+    <td>
+    	<img src = {icon} width="200px" height="200px" alt="image of team member"></img>
+    </td>
+
+  </tr>
+</table>
+
+  </body>
+  );
 }
+
+
