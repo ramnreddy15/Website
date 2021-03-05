283bf8eee63529ee6a1f6f67d43ba403503ed371
Aditya Vasantharao <hsfan612@gmail.com>
2021-03-05 01:08:31 -0500

    Add media queries for more devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,15 @@ import hero from "./assets/img/olympian_hero.png";
 
 export default function Home(props) {
   return (
-    <main className="hero">
+    <body>
+      <section className="hero">
       <div className="landing-header">
         <h1 id="potentia">Potentia</h1>
         <h1 id="robotics">Robotics</h1>
       </div>
       <div className="landing-subheader">
         <p>
-        The first high school team to build a full-size bipedal humanoid robot.
+        The first high school team to design and build a full-size bipedal humanoid robot.
         </p>
       </div>
       <img
@@ -20,7 +21,23 @@ export default function Home(props) {
         src={hero}
         alt=""
       />    
-      
-    </main>
+      </section>
+      {/* <section className="intro-section">
+        <div className="intro-wrapper">
+          <div className="intro-content">
+            <h1>Who are we?</h1>
+            <p>
+              Content
+            </p>
+          </div>
+          <div className="mission-statement">
+            <h1>Our mission</h1>
+            <p>
+              Content
+            </p>
+          </div>
+        </div>
+      </section> */}
+    </body>
   );
 }
