54da7acecdbc9ef5b8e00fcf73b72e8ca27f8fa2
audhavdurai <60197772+audhavdurai@users.noreply.github.com>
2021-03-12 21:27:37 -0500

    Update Events.js

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,26 +1,32 @@
-import React from "react";
 import "./assets/Events.scss";
-import hero from "./assets/img/olympian_hero.png";
+import React, {Component} from "react";
+import {createGlobalStyle} from 'styled-components';
+import bot2 from "./assets/img/bot2.png";
+import './assets/Donate.scss'
+//var Diamond = require("./assets/img/0002.jpg")
+import icon from "./assets/img/0002.jpg";
+import icon2 from "./assets/img/0001.jpg";
+import icon3 from "./assets/img/0003.jpg";
+import pdf from "./assets/Python_BootCamp.pdf";
 
-export default function Events(props) {
-  return (
-    <body>
-      <section className="hero-temp">
-      <div className="landing-header">
-        <h1 id="potentia"><span className="highlight-gray">&nbsp;Potentia&nbsp;</span></h1>
-        <h1 id="robotics"><span className="highlight-gray">&nbsp;Robotics&nbsp;</span></h1>
-      </div>
-      <div className="landing-subheader">
-        <p>
-        <span className="highlight-gray">&nbsp;This page is under construction.&nbsp;<br></br>&nbsp;Please check back later!&nbsp;</span>
-        </p>
-      </div>
-      <img
-        className="landing-robot"
-        src={hero}
-        alt=""
-      />    
-      </section>
-    </body>
-  );
+
+class Events extends Component {
+  render() {
+      return(
+          <>
+          <br></br>
+          <div style={{marginLeft: '35%'}}>
+            <img src = {icon2} width="50%" height="100%" alt="image of team member"></img>
+          </div>
+          
+          <div style={{marginLeft: '45%'}}>
+            <a href={pdf} download>Click to Download Information</a>
+          </div>
+          
+          </>
+      );
+  }
 }
+
+
+export default Events;
